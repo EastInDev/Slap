2bddc08d57a7cf788b7c17ec799f71358884e6f1
Hyunjin <aihaoyi12@gmail.com>
2024-02-02 11:23:01 +0900

    폰트 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,10 @@
-import { Inter } from 'next/font/google'
+import { IBM_Plex_Sans_KR } from 'next/font/google'
 import '@/app/globals.css'
 import Providers from '@/features/home/Providers/Providers'
 import Intro from '@/features/home/Intro/Intro'
 import Header from '@/components/home/Header/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const ibmPlexSansKR = IBM_Plex_Sans_KR({ subsets: ['latin'], weight: '400' })
 
 export const metadata = {
   title: 'Slap',
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${ibmPlexSansKR.className} p-5`}>
         <Intro />
         <Providers>
           <Header />
