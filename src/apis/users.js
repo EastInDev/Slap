e960207646cfd19f7a45838fd17d14cdf1412546
HyunJin <49902665+Hyunjin012@users.noreply.github.com>
2024-02-02 15:49:40 +0900

    로그인 시 자동 회원가입 (#1)

---
+++ src/apis/users.js
@@ -0,0 +1,38 @@
+import { sql } from '@vercel/postgres'
+
+export const getUser = async (id, type) => {
+  try {
+    const { rows } = await sql`
+      SELECT * FROM users WHERE sns_id = ${id} AND sns_type = ${type}
+    `
+
+    return rows
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+export const createUser = async (user, account) => {
+  'use server'
+  try {
+    await sql`
+      INSERT INTO users (sns_id, sns_type, nickname, role)
+      VALUES (${user.id}, ${account.provider}, ${user.name}, '사용자')
+    `
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+export const loginUser = async (user, account) => {
+  'use server'
+  try {
+    await sql`
+      UPDATE users 
+      set last_login_time = now(), refresh_token = ${account.refresh_token} 
+      WHERE sns_id = ${user.id} and sns_type = ${account.provider}
+    `
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
