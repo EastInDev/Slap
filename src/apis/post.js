6820feed8b72f46940c8c38f577b4aa781df53cb
Dong In Kang <ehddls020624@gmail.com>
2024-02-04 16:29:22 +0900

    투표 참여

--- src/apis/post.js
+++ src/apis/post.js
@@ -1,7 +1,6 @@
 'use server'
 
 import { sql } from '@vercel/postgres'
-import { useSession } from 'next-auth/react'
 
 export const createPost = async ({
   voteText,
@@ -13,7 +12,7 @@ export const createPost = async ({
   try {
     const { rows } = await sql`
       INSERT INTO posts (title, content, created_at, updated_at, category_id, user_id)
-      VALUES (${voteText}, ${content}, NOW(), NOW(), ${category}, 1)
+      VALUES (${voteText}, ${content}, NOW(), NOW(), ${category}, ${userId})
       RETURNING id, title, content, created_at, updated_at
     `
 
@@ -25,6 +24,74 @@ export const createPost = async ({
     })
   } catch (error) {
     console.error('포스트 생성 실패:', error)
+    return null
+  }
+}
+
+export const getPosts = async () => {
+  try {
+    const { rows } = await sql`
+      SELECT 
+        posts.id,
+        posts.title,
+        posts.content,
+        posts.created_at,
+        posts.updated_at,
+        posts.category_id,
+        posts.user_id,
+        users.nickname,
+        votes.id AS vote_id,
+        votes.text AS vote_text,
+        votes.post_id,
+        COUNT(slaps.id) OVER (PARTITION BY votes.id) AS vote_count,
+        COUNT(slaps.id) OVER (PARTITION BY posts.id) AS total_vote_count
+      FROM 
+        posts
+      LEFT JOIN 
+        votes
+      ON 
+        posts.id = votes.post_id
+      LEFT JOIN 
+        users
+      ON 
+        posts.user_id = users.id
+      LEFT JOIN
+        slaps
+      ON
+        votes.id = slaps.vote_id
+    `
+    return rows
+  } catch (error) {
+    console.error('포스트 조회 실패:', error)
+    return null
+  }
+}
+
+export const addVote = async (post_id, vote_id, user_id) => {
+  try {
+    const { rows } = await sql`
+      SELECT * FROM slaps
+      WHERE post_id = ${post_id} AND user_id = ${user_id}
+    `
+
+    if (rows.length > 0) {
+      console.log('이전 투표를 삭제하고 새로운 투표를 추가합니다.')
+
+      await sql`
+        DELETE FROM slaps
+        WHERE post_id = ${post_id} AND user_id = ${user_id}
+      `
+    }
+
+    await sql`
+      INSERT INTO slaps (post_id, vote_id, user_id, updated_at)
+      VALUES (${post_id}, ${vote_id}, ${user_id}, NOW())
+    `
+
+    console.log('투표에 성공하였습니다.')
+    return true
+  } catch (error) {
+    console.error('투표 추가 실패:', error)
     return null
   }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Categories from '@/features/home/Categories/Categories'
+import GetPost from '@/features/post/GetPost/GetPost'
 
 export default function Home() {
   return (
     <>
       <Categories />
+      <br></br>
+      <GetPost />
     </>
   )
 }
