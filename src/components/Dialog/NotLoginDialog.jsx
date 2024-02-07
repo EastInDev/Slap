83970232fa4b5a489525013196754a073d951793
Dong In Kang <ehddls020624@gmail.com>
2024-02-07 16:14:56 +0900

    로그인 경고 메시지 수정

--- src/components/Dialog/NotLoginDialog.jsx
+++ src/components/Dialog/NotLoginDialog.jsx
@@ -2,8 +2,8 @@ const NotLoginDialog = () => {
   return (
     <dialog id="NotLoginDialog" className="modal">
       <div className="modal-box">
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click outside to close</p>
+        <h3 className="font-bold text-lg">메시지</h3>
+        <p className="py-4">로그인 후 투표하실 수 있어요!</p>
       </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
