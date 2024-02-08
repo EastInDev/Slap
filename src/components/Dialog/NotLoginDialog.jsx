1669ec472e0e6ec99f7cacd72db0e1fcc43c3515
HyunJin <aihaoyi12@gmail.com>
2024-02-08 09:59:27 +0900

    dialog 메세지 변경

--- src/components/Dialog/NotLoginDialog.jsx
+++ src/components/Dialog/NotLoginDialog.jsx
@@ -3,7 +3,7 @@ const NotLoginDialog = () => {
     <dialog id="NotLoginDialog" className="modal">
       <div className="modal-box">
         <h3 className="font-bold text-lg">메시지</h3>
-        <p className="py-4">로그인 후 투표하실 수 있어요!</p>
+        <p className="py-4">로그인 후 이용하실 수 있어요!</p>
       </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
