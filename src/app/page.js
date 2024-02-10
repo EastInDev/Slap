f387a2d05c29681364ad38794a854993793b76a1
HyunJin <49902665+Hyunjin012@users.noreply.github.com>
2024-02-10 12:41:31 +0900

    Revert "Feat/post options (#8)" (#9)
    This reverts commit 8356f8da67f711bb6ba9f748c5baff7ba5544a5d.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
 import Categories from '@/features/home/Categories/Categories'
-import MainPost from '@/features/post/MainPost/MainPost'
 
 export default function Home() {
   return (
     <>
       <Categories />
-      <br></br>
-      <MainPost />
     </>
   )
 }
--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,19 +1,13 @@
-'use client'
-
 import Search from '@/features/layout/Search/Search'
 import Logo from '../Logo/Logo'
 import Profile from '@/features/layout/Profile/Profile'
-import Icons from '@/features/layout/Icons/Icons'
 
 const Header = () => {
   return (
-    <header className="flex justify-between h-20">
+    <header className="flex justify-between h-18">
       <Logo />
       <Search />
-      <div className="flex align-middle">
-        <Icons />
-        <Profile />
-      </div>
+      <Profile />
     </header>
   )
 }
